2aa20f588cf788c7a63e5d23f33c8ccd774a6ae1
Max <max.andreas.schmitt@googlemail.com>
2016-05-01 18:44:06 +0200

    added enqueue

--- youtube.js
+++ youtube.js
@@ -13,21 +13,20 @@ registerPlugin({
 }, function(sinusbot,config, info){
 sinusbot.log("YTWeb Webinterface Ready");
     sinusbot.on('api:ytplay', function(ev) {
-    sinusbot.log('YTWeb Triggered with "played" at '+ ev.data);  
-    sinusbot.yt(ev.data);  
+        sinusbot.yt(ev.data);
+        sinusbot.log('YTWeb Triggered with "played" at '+ ev.data);    
         return 'The Video will be sucessfully played now.';
         
 });
     sinusbot.on('api:ytenq', function(ev) {
-    // sinusbot.log('YTWeb Triggered with "enque" at '+ ev.data);    
-    // return 'The Video will be sucessfully enqueued now.';   
-        sinusbot.log('YTWeb Enqueuing is not available yet');    
-        return 'YTWeb Enqueuing is not available yet';   
+        qyt(ev.data);
+        sinusbot.log('YTWeb Triggered with "enque" at '+ ev.data);    
+        return 'The Video will be sucessfully enqueued now.';   
         
 });
     sinusbot.on('api:ytdl', function(ev) {
-    sinusbot.log('YTWeb Triggered with "downloaded" at '+ ev.data); 
-    sinusbot.ytdl(ev.data); 
+        sinusbot.log('YTWeb Triggered with "downloaded" at '+ ev.data); 
+        sinusbot.ytdl(ev.data); 
         return 'The Video will be sucessfully downloaded now.';
         
 });
