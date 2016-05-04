aa7ef704fdb64bc1874a77c93bc4aab92d5c8fce
Jonas Bögle <l@sandstorm-projects.de>
2016-05-04 22:15:34 +0200

    fixed config check #2

--- youtube/html/src/js/main.js
+++ youtube/html/src/js/main.js
@@ -76,13 +76,15 @@ $(document).ready(function() {
         data: "{}"
       }).done(function(data) {
         data.forEach(function(answer) {
-          if(typeof config == 'undefined' && typeof config.apikey == 'undefined') {
-            console.log("answer:");
-            console.log(answer.data);
-            config = answer.data;
-            if(ytapi_ready) {
-              console.log("init #2");
-              ytinit();
+          if (typeof config == 'undefined') {
+            if (typeof config.apikey == 'undefined') {
+              console.log("answer:");
+              console.log(answer.data);
+              config = answer.data;
+              if(ytapi_ready) {
+                console.log("init #2");
+                ytinit();
+              }
             }
           }
         });
