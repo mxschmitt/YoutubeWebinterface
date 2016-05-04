ad3e2fc8b1317a4e38609b1751ff21ef0a054254
Max <max.andreas.schmitt@googlemail.com>
2016-05-04 21:44:53 +0200

    beautifyed

--- youtube/html/src/js/main.js
+++ youtube/html/src/js/main.js
@@ -3,7 +3,7 @@ var nextPageToken, instanceid, config, ytapi_ready = false;
 
 function tplawesome(e, t) {
   res = e;
-  for (var n = 0; n < t.length; n++) {
+  for(var n = 0; n < t.length; n++) {
     res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) {
       return t[n][r];
     });
@@ -17,12 +17,10 @@ function resetVideoHeight() {
 
 function ytinit() {
   ytapi_ready = true;
-
   console.log("config:");
   console.log(config);
-
-  if (typeof config != 'undefined') {
-    if (config.apikey.length == 39) {
+  if(typeof config != 'undefined') {
+    if(config.apikey.length == 39) {
       gapi.client.setApiKey(config.apikey);
       gapi.client.load("youtube", "v3", function() {});
       delete config.apikey;
@@ -34,7 +32,6 @@ function ytinit() {
     console.log("config is undefined, trying again later.");
   }
 }
-
 //YT END Start Sinusbot Connection
 $(document).ready(function() {
   var instanceList = $('#dropdown');
@@ -63,10 +60,9 @@ $(document).ready(function() {
     data.forEach(function(instance) {
       // When clicking an entry, we post a request to the script interface and return the result
       $('<li/>').appendTo(instanceList).html('<a href="#">' + instance.nick + '</a>').click(function() {
-        setCookie('instanceid',instance.uuid,7);
+        setCookie('instanceid', instance.uuid, 7);
         instanceid = instance.uuid;
       });
-      
       $.ajax({
         url: '/api/v1/bot/i/' + instance.uuid + '/scriptEvent/ytwebconfig',
         method: 'POST',
@@ -79,42 +75,34 @@ $(document).ready(function() {
         data.forEach(function(answer) {
           console.log("answer:");
           console.log(answer.data);
-
           config = answer.data;
-
-          if (ytapi_ready) {
+          if(ytapi_ready) {
             console.log("init #2");
             ytinit();
           }
         });
       });
     });
-    
     $('.dropd1 > li > a').click(function() {
       $("#dropdb1").html($(this).text() + ' <span class="caret"></span>');
     });
-    
     checkForInstance(data);
   });
-  
-  $(window).scroll(function(){
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-      if (typeof nextPageToken !== 'undefined') {
+  $(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+      if(typeof nextPageToken !== 'undefined') {
         moreVideos();
       }
     }
   });
-  
   $("#loadmore").click(function() {
-    if (typeof nextPageToken !== 'undefined') {
+    if(typeof nextPageToken !== 'undefined') {
       moreVideos();
     }
   });
-
   $("form").on("submit", function(e) {
     e.preventDefault();
-
-    if ($("#search").val()) {
+    if($("#search").val()) {
       // prepare the request
       var request = gapi.client.youtube.search.list({
         part: "snippet",
@@ -140,14 +128,11 @@ $(document).ready(function() {
         $("#loadmore").css("display", "block");
       });
     } else {
-        noSelectedVideo();
+      noSelectedVideo();
     }
   });
-  
   $(window).on("resize", resetVideoHeight);
-
 }); //Document Ready End
-
 function endplay(url) {
   if(typeof instanceid !== 'undefined') {
     $.ajax({
@@ -169,14 +154,14 @@ function endplay(url) {
       data.forEach(function(answer) {
         $('<li/>').appendTo(answerList).text(answer.script + ' returned ' + JSON.stringify(answer.data));
       });
-        if (data[0].data.includes('not enabled')) {
-          createAlertBox('failure',data[0].data);
+      if(data[0].data.includes('not enabled')) {
+        createAlertBox('failure', data[0].data);
       } else {
-          createAlertBox('success',data[0].data);
+        createAlertBox('success', data[0].data);
       }
     });
   } else {
-      selectInstance();
+    selectInstance();
   }
 }
 
@@ -201,14 +186,14 @@ function endenqueue(url) {
       data.forEach(function(answer) {
         $('<li/>').appendTo(answerList).text(answer.script + ' returned ' + JSON.stringify(answer.data));
       });
-      if (data[0].data.includes('not enabled')) {
-          createAlertBox('failure',data[0].data);
+      if(data[0].data.includes('not enabled')) {
+        createAlertBox('failure', data[0].data);
       } else {
-          createAlertBox('success',data[0].data);
+        createAlertBox('success', data[0].data);
       }
     });
   } else {
-      selectInstance();
+    selectInstance();
   }
 }
 
@@ -233,14 +218,14 @@ function enddownload(url) {
       data.forEach(function(answer) {
         $('<li/>').appendTo(answerList).text(answer.script + ' returned ' + JSON.stringify(answer.data));
       });
-            if (data[0].data.includes('not enabled')) {
-          createAlertBox('failure',data[0].data);
+      if(data[0].data.includes('not enabled')) {
+        createAlertBox('failure', data[0].data);
       } else {
-          createAlertBox('success',data[0].data);
+        createAlertBox('success', data[0].data);
       }
     });
   } else {
-      selectInstance();
+    selectInstance();
   }
 }
 
@@ -249,43 +234,43 @@ function getRootUrl() {
 }
 
 function checkForInstance(data) {
-    if (checkCookie('instanceid') == true) {
-        i = 0;
-        data.forEach(function(instance) {
-            if (instance.uuid == getCookie('instanceid')) {
-                instanceid = data[i].uuid;
-                $("#dropdb1").html(data[i].nick);
-            }
-            i++;
-        });
-    }
+  if(checkCookie('instanceid') == true) {
+    i = 0;
+    data.forEach(function(instance) {
+      if(instance.uuid == getCookie('instanceid')) {
+        instanceid = data[i].uuid;
+        $("#dropdb1").html(data[i].nick);
+      }
+      i++;
+    });
+  }
 }
 
 function moreVideos() {
-    $("#loadmorecss").removeClass("none");
-    var request = gapi.client.youtube.search.list({
-      part: "snippet",
-      type: "video",
-      q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-      maxResults: maxResults,
-      pageToken: nextPageToken,
-      order: "relevance"
-    });
-    // execute the request
-    request.execute(function(response) {
-      var result = response.result;
-      nextPageToken = result.nextPageToken;
-      $.each(result.items, function(index, item) {
-        $.get("src/yt.html", function(data) {
-          $("#results").append(tplawesome(data, [{
-            "title": item.snippet.title,
-            "videoid": item.id.videoId
-          }]));
-        });
+  $("#loadmorecss").removeClass("none");
+  var request = gapi.client.youtube.search.list({
+    part: "snippet",
+    type: "video",
+    q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+    maxResults: maxResults,
+    pageToken: nextPageToken,
+    order: "relevance"
+  });
+  // execute the request
+  request.execute(function(response) {
+    var result = response.result;
+    nextPageToken = result.nextPageToken;
+    $.each(result.items, function(index, item) {
+      $.get("src/yt.html", function(data) {
+        $("#results").append(tplawesome(data, [{
+          "title": item.snippet.title,
+          "videoid": item.id.videoId
+        }]));
       });
-      resetVideoHeight();
-      $("#loadmorecss").addClass("none");
     });
+    resetVideoHeight();
+    $("#loadmorecss").addClass("none");
+  });
 }
 
 function dynamicSort(property) {
@@ -303,61 +288,65 @@ function dynamicSort(property) {
 function enablePlugin() {
   sweetAlert('Failed...', "Be sure, that you've enabled the script in your Webinterface!", 'error')
 }
+
 function selectInstance() {
   sweetAlert('Failed...', "Be sure, that you select an instance!", 'error')
 }
+
 function noSelectedVideo() {
   sweetAlert('Failed...', "Be sure, that you´ve entered a search term!", 'error')
 }
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
 }
+
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while(c.charAt(0) == ' ') c = c.substring(1);
+    if(c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
 }
+
 function checkCookie(cname) {
-    var user = getCookie(cname);
-    if (user != "") {
-       return true;
-    } else {
-        return false;
-    }
+  var user = getCookie(cname);
+  if(user != "") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function createAlertBox(type,text) {
-    if (type == 'success') {
-      $('div > #alertscss').removeClass("alert-danger");
-      $('div > #alertscss').removeClass("alert-warning");
-      $('div > #alertscss').addClass("alert-success");
-      $('#alertscss > #title').text("Success!");
-      $('#alertscss > #text').text(text);
-      $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
-    } else if (type == 'failure') {
-      $('div > #alertscss').removeClass("alert-warning");
-      $('div > #alertscss').removeClass("alert-success");
-      $('div > #alertscss').addClass("alert-danger");
-      $('#alertscss > #title').text("Failure!");
-      $('#alertscss > #text').text(text);
-      $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
-    } else if (type == 'warning') {
-      $('div > #alertscss').removeClass("alert-danger");
-      $('div > #alertscss').removeClass("alert-success");
-      $('div > #alertscss').addClass("alert-warning");
-      $('#alertscss > #title').text("Warning!");
-      $('#alertscss > #text').text(text);
-      $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
-    } else {
-      return false;
-    }
-
+function createAlertBox(type, text) {
+  if(type == 'success') {
+    $('div > #alertscss').removeClass("alert-danger");
+    $('div > #alertscss').removeClass("alert-warning");
+    $('div > #alertscss').addClass("alert-success");
+    $('#alertscss > #title').text("Success!");
+    $('#alertscss > #text').text(text);
+    $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
+  } else if(type == 'failure') {
+    $('div > #alertscss').removeClass("alert-warning");
+    $('div > #alertscss').removeClass("alert-success");
+    $('div > #alertscss').addClass("alert-danger");
+    $('#alertscss > #title').text("Failure!");
+    $('#alertscss > #text').text(text);
+    $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
+  } else if(type == 'warning') {
+    $('div > #alertscss').removeClass("alert-danger");
+    $('div > #alertscss').removeClass("alert-success");
+    $('div > #alertscss').addClass("alert-warning");
+    $('#alertscss > #title').text("Warning!");
+    $('#alertscss > #text').text(text);
+    $('#alertscss').fadeIn(400).delay(2000).fadeOut(400);
+  } else {
+    return false;
+  }
 }
