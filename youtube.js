f021d140692405c9e1474104f56a2f4acf3433fd
Max <max.andreas.schmitt@googlemail.com>
2016-10-06 08:38:00 +0200

    beautified

--- youtube.js
+++ youtube.js
@@ -9,25 +9,25 @@ registerPlugin({
             type: 'string'
         },
         play: {
-            title: 'enable playing', 
-            type: 'select', 
-            options: ['on','off']
+            title: 'enable playing',
+            type: 'select',
+            options: ['on', 'off']
         },
         dl: {
             title: 'enable downloading',
             type: 'select',
-            options: ['on','off']
+            options: ['on', 'off']
         },
         enq: {
             title: 'enable enqueuing',
             type: 'select',
-            options: ['on','off']
+            options: ['on', 'off']
         }
     },
     enableWeb: true
 }, function(sinusbot, config, info) {
-    
-sinusbot.registerHandler({
+
+    sinusbot.registerHandler({
         isHandlerFor: function(url) {
             if (url.substring(0, 4) == 'ytdl') {
                 sinusbot.log(url);
@@ -43,12 +43,21 @@ sinusbot.registerHandler({
         getTrackInfo: function(url, cb) {
             sinusbot.log(url);
             if (/youtube\.com/.test(url)) {
-                return cb({ urlType: 'ytdl', url: url });
+                return cb({
+                    urlType: 'ytdl',
+                    url: url
+                });
             }
-            return cb({ urlType: 'ytdl', url: url.substring(17) });
+            return cb({
+                urlType: 'ytdl',
+                url: url.substring(17)
+            });
         },
         getSearchResult: function(search, cb) {
-            http({ timeout: 5000, url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + encodeURIComponent(search) + '&maxResults=20&type=video,playlist&key=' + encodeURIComponent(config.apikey) }, function(err, data) {
+            http({
+                timeout: 5000,
+                url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + encodeURIComponent(search) + '&maxResults=20&type=video,playlist&key=' + encodeURIComponent(config.apikey)
+            }, function(err, data) {
                 if (err || !data || data.statusCode != 200) {
                     return cb(null);
                 }
@@ -69,43 +78,43 @@ sinusbot.registerHandler({
                 return cb(result);
             });
         }
-});
+    });
 
     sinusbot.log("YTWeb Webinterface Ready");
 
     if (typeof config.apikey != 'undefined') {
         sinusbot.setVar("ytapikey", config.apikey);
     }
-    
+
     sinusbot.on('api:ytplay', function(ev) {
         if (config.play != 1) {
             sinusbot.yt(ev.data);
-            sinusbot.log('YTWeb Triggered with "played" at '+ ev.data);    
+            sinusbot.log('YTWeb Triggered with "played" at ' + ev.data);
             return 'The Video will be sucessfully played now.';
         } else {
-            sinusbot.log('YTWeb tried to play '+ ev.data + ' but it was deactivated.'); 
-            return 'Playing is not enabled.';            
-        }    
+            sinusbot.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
+            return 'Playing is not enabled.';
+        }
     });
-    
+
     sinusbot.on('api:ytenq', function(ev) {
         if (config.enq != 1) {
             sinusbot.qyt(ev.data);
-            sinusbot.log('YTWeb Triggered with "enque" at '+ ev.data);    
+            sinusbot.log('YTWeb Triggered with "enque" at ' + ev.data);
             return 'The Video will be sucessfully enqueued now.';
         } else {
-            sinusbot.log('YTWeb tried to play '+ ev.data + ' but it was deactivated.'); 
+            sinusbot.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
             return 'Enqueuing is not enabled.';
         }
     });
 
     sinusbot.on('api:ytdl', function(ev) {
         if (config.dl != 1) {
-            sinusbot.log('YTWeb Triggered with "downloaded" at '+ ev.data);
+            sinusbot.log('YTWeb Triggered with "downloaded" at ' + ev.data);
             sinusbot.ytdl(ev.data);
             return 'The Video will be sucessfully downloaded now.';
         } else {
-            sinusbot.log('YTWeb tried to download '+ ev.data + ' but it was deactivated.');
+            sinusbot.log('YTWeb tried to download ' + ev.data + ' but it was deactivated.');
             return 'Downloading is not enabled.';
         }
     });
