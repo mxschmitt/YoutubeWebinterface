e78947a361a1b9d6cac082b86c8c8b6ea99eb8f3
Max <max.andreas.schmitt@googlemail.com>
2016-11-04 20:44:33 +0100

    Updated to latest Script Engine

--- youtube.js
+++ youtube.js
@@ -34,18 +34,18 @@ registerPlugin({
         sinusbot.registerHandler({
             isHandlerFor: function(url) {
                 if (url.substring(0, 6) == 'ytwiyt') {
-                    sinusbot.log(url);
+                    engine.log(url);
                     return true;
                 }
                 if (/youtube\.com/.test(url)) {
-                    sinusbot.log(url);
+                    engine.log(url);
                     return true;
                 }
-                sinusbot.log('NOPE: ' + url);
+                engine.log('NOPE: ' + url);
                 return false;
             },
             getTrackInfo: function(url, cb) {
-                sinusbot.log(url);
+                engine.log(url);
                 if (/youtube\.com/.test(url)) {
                     return cb({
                         urlType: 'ytdl',
@@ -58,7 +58,7 @@ registerPlugin({
                 });
             },
             getSearchResult: function(search, cb) {
-                sinusbot.log("Youtube triggered.");
+                engine.log("Youtube triggered.");
                 http({
                     timeout: 5000,
                     url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + encodeURIComponent(search) + '&maxResults=20&type=video,playlist&key=' + encodeURIComponent(config.apikey)
@@ -69,7 +69,7 @@ registerPlugin({
                     var result = [];
                     var data = JSON.parse(data.data);
                     if (!data || !data.items) {
-                        sinusbot.log('Error in json');
+                        engine.log('Error in json');
                         return cb(null);
                     }
                     data.items.forEach(function(entry) {
@@ -85,7 +85,7 @@ registerPlugin({
             }
         });
     } else {
-        sinusbot.log("Youtube API Key is not set, so it isn't working atm.");
+        engine.log("Youtube API Key is not set, so it isn't working atm.");
     }
 
 
@@ -98,7 +98,7 @@ registerPlugin({
                 if (/soundcloud\.com/.test(url)) {
                     return true;
                 }
-                sinusbot.log('Soundcloud: NOPE: ' + url);
+                engine.log('Soundcloud: NOPE: ' + url);
                 return false;
             },
             getTrackInfo: function(url, cb) {
@@ -110,7 +110,7 @@ registerPlugin({
                 }
             },
             getSearchResult: function(search, cb) {
-                sinusbot.log("Soundcloud triggered");
+                engine.log("Soundcloud triggered");
                 http({
                     timeout: 5000,
                     url: 'http://api.soundcloud.com/tracks.json?client_id=' + config.scApiKey + '&q=' + encodeURIComponent(search) + '&limit=20'
@@ -121,7 +121,7 @@ registerPlugin({
                     var result = [];
                     var data = JSON.parse(data.data);
                     if (!data) {
-                        sinusbot.log('Error in json');
+                        engine.log('Error in json');
                         return cb(null);
                     }
                     data.forEach(function(entry) {
@@ -137,45 +137,49 @@ registerPlugin({
             }
         });
     } else {
-        sinusbot.log("Soundcloud API Key is not set, so it isn't working atm.");
+        engine.log("Soundcloud API Key is not set, so it isn't working atm.");
     }
 
+var engine = require('engine');
+var store = require('store');
+var event = require('event');
+var media = require('media');
 
-    sinusbot.log("YTWeb Webinterface Ready");
+    engine.log("YTWeb Webinterface Ready");
 
     if (typeof config.ytApiKey != 'undefined') {
-        sinusbot.setVar("ytapikey", config.ytApiKey);
+        store.set("ytapikey", config.ytApiKey);
     }
 
-    sinusbot.on('api:ytplay', function(ev) {
+    event.on('api:ytplay', function(ev) {
         if (config.play != 1) {
-            sinusbot.yt(ev.data);
-            sinusbot.log('YTWeb Triggered with "played" at ' + ev.data);
+            media.yt(ev.data);
+            engine.log('YTWeb Triggered with "played" at ' + ev.data);
             return 'The Video will be sucessfully played now.';
         } else {
-            sinusbot.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
+            engine.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
             return 'Playing is not enabled.';
         }
     });
 
-    sinusbot.on('api:ytenq', function(ev) {
+    event.on('api:ytenq', function(ev) {
         if (config.enq != 1) {
-            sinusbot.qyt(ev.data);
-            sinusbot.log('YTWeb Triggered with "enque" at ' + ev.data);
+            media.qyt(ev.data);
+            engine.log('YTWeb Triggered with "enque" at ' + ev.data);
             return 'The Video will be sucessfully enqueued now.';
         } else {
-            sinusbot.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
+            engine.log('YTWeb tried to play ' + ev.data + ' but it was deactivated.');
             return 'Enqueuing is not enabled.';
         }
     });
 
-    sinusbot.on('api:ytdl', function(ev) {
+    event.on('api:ytdl', function(ev) {
         if (config.dl != 1) {
-            sinusbot.log('YTWeb Triggered with "downloaded" at ' + ev.data);
-            sinusbot.ytdl(ev.data);
+            media.ytdl(ev.data, false);
+            engine.log('YTWeb Triggered with "downloaded" at ' + ev.data);
             return 'The Video will be sucessfully downloaded now.';
         } else {
-            sinusbot.log('YTWeb tried to download ' + ev.data + ' but it was deactivated.');
+            engine.log('YTWeb tried to download ' + ev.data + ' but it was deactivated.');
             return 'Downloading is not enabled.';
         }
     });
@@ -185,7 +189,7 @@ registerPlugin({
         ytwebconfig.play = (config.play != 1);
         ytwebconfig.enqueue = (config.enq != 1);
         ytwebconfig.download = (config.dl != 1);
-        ytwebconfig.ytapikey = sinusbot.getVar("ytapikey");
+        ytwebconfig.ytapikey = store.get("ytapikey");
         return ytwebconfig;
     });
 });
